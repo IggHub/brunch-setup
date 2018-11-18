768107493acf985a01c41c1f7e0773b172f47e2e
Iggy <igoririanto@rocketmail.com>
2018-11-17 19:38:17 -0600

    first commit proj

---
+++ app/initialize.js
@@ -0,0 +1,6 @@
+var $ = require('jquery');
+console.log('Tasty Brunch, jQuery for elevensies!', $('body'));
+document.addEventListener('DOMContentLoaded', () => {
+  // do your setup here
+  console.log('Initialized app');
+});
